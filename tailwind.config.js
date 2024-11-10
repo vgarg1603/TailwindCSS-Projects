a486f313f1e017a67dd8abbb55fe0d6156bc0cd0
vgarg1603 <vgarg1603@gmail.com>
2024-11-10 22:53:29 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      zIndex: {
+        '1000': '1000',
+      }
+    },
+  },
+  plugins: [],
+}
+
